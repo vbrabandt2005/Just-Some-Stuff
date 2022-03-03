c704e9ec12953e967b26f17c44e38670511c5f88
TheGoogleRider2 <vbrabandt2@gmail.com>
2022-03-04 00:36:37 +0100

    Update TemperatureCalc.py

--- TemperatureCalc.py
+++ TemperatureCalc.py
@@ -1,9 +1,21 @@
-
+# name: TemperatureCalc
+# description:
+# Simple Temperature Converter in Python
+#
+# 
+# author: @TheGoogleRider
+# version: 1.0
+# date: 04/03/2022
 
 
 c = float(input("Input Celsius: "))
-
+print()
+print("Celsius Input = ",c
+)
+#Celsius to Kelvin
 k = c + 273.15 
 print("Kelvin = ",k)
 
-
+#Celsius to Fahrenheit
+f = c * 9/5 + 32
+print("Fahrenheit = ",f)
