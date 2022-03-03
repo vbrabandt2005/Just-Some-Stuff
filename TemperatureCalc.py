dd8ff69b9ecc4860f3ee57e5417931e5f7b10459
TheGoogleRider2 <vbrabandt2@gmail.com>
2022-03-04 00:51:48 +0100

    Update TemperatureCalc.py

--- TemperatureCalc.py
+++ TemperatureCalc.py
@@ -7,15 +7,28 @@
 # version: 1.0
 # date: 04/03/2022
 
+from secrets import choice
 
-c = float(input("Input Celsius: "))
-print()
-print("Celsius Input = ",c
-)
-#Celsius to Kelvin
-k = c + 273.15 
-print("Kelvin = ",k)
 
-#Celsius to Fahrenheit
-f = c * 9/5 + 32
-print("Fahrenheit = ",f)
+n = input("Enter the unit you want to convert: ")
+
+for i in range(0,n):
+    print("Enter '1' for Celsius\n")
+    print("Enter '2' for Fahrenheit\n")
+    print("Enter '3' for Kelvin\n")
+
+    if (choice == 1):
+        c = float(input("Input Celsius: "))
+        print()
+        print("Celsius Input = ",c)
+        #Celsius to Kelvin
+        k = c + 273.15 
+        print("Kelvin = ",k)
+        #Celsius to Fahrenheit
+        f = c * 9/5 + 32 
+        print("Fahrenheit = ",f)
+    if (choice == 2):
+        print("pp")
+    if (choice == 3):
+        print("PooPoo")    
+          
