716a9f14f2e474900ad3f464138db71f7c97249e
TheGoogleRider2 <vbrabandt2@gmail.com>
2022-09-06 02:30:40 +0200

    a

---
+++ Cyptography Programs/Subtitution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Subtitution{
+    public static void public static void main(String[] args) 
+        {
+            String Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            String Key = "";
+            String Plain_Text = "";
+            String Encrypted = "";
+
+            for 
+        }
+    }
